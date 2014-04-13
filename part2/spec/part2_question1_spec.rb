7521476a7b81c2780535e9f0898c6232f7321745
Rick Winfrey <rick.winfrey@gmail.com>
2014-04-13 16:01:44 -0500

    Adds Part2 Question1 parse date range method

--- part2/spec/part2_question1_spec.rb
+++ part2/spec/part2_question1_spec.rb
@@ -4,12 +4,20 @@ def parse_price(input_string)
   /\d+\.\d+$/.match(input_string).to_s.to_f
 end
 
+def parse_date_range(input_string)
+  /\d+\/\d+\s\-\s\d+\/\d+/.match(input_string).to_s
+end
+
 describe 'part2_question1' do
   context 'parsing' do
     let(:example) { "$5.00 TXT MESSAGING - 250 09/29 - 10/28 4.99" }
 
     it 'parses the price found at the end of a string' do
       parse_price(example).should == 4.99
+    end
+
+    it 'parses the date range from the line' do
+      parse_date_range(example).should == "09/29 - 10/28"
     end
   end
 end
