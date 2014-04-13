d3c66b841eabe14a2407605362879bfde38add2a
Rick Winfrey <rick.winfrey@gmail.com>
2014-04-13 16:05:48 -0500

    Adds Part2 Question1 parse feature method

--- part2/spec/part2_question1_spec.rb
+++ part2/spec/part2_question1_spec.rb
@@ -8,16 +8,24 @@ def parse_date_range(input_string)
   /\d+\/\d+\s\-\s\d+\/\d+/.match(input_string).to_s
 end
 
+def parse_feature(input_string)
+  /[A-Z]+\s[A-Z]+\s\-\s\d+/.match(input_string).to_s
+end
+
 describe 'part2_question1' do
   context 'parsing' do
     let(:example) { "$5.00 TXT MESSAGING - 250 09/29 - 10/28 4.99" }
 
-    it 'parses the price found at the end of a string' do
+    it 'parses the price found at the end of an input string' do
       parse_price(example).should == 4.99
     end
 
-    it 'parses the date range from the line' do
+    it 'parses the date range from the input string' do
       parse_date_range(example).should == "09/29 - 10/28"
+    end
+
+    it 'parses the feature from the input string' do
+      parse_feature(example).should == "TXT MESSAGING - 250"
     end
   end
 end
