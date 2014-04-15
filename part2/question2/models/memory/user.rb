822bc3c4db1e3db7fc3094ff209ac9c5bdf67a00
Rick Winfrey <rick.winfrey@gmail.com>
2014-04-14 23:29:20 -0500

    Adds revoke and add logic for Models::Memory::User organizations and roles

--- part2/question2/models/memory/user.rb
+++ part2/question2/models/memory/user.rb
@@ -1,10 +1,7 @@
-require 'hashie'
-
 module Models
   module Memory
     class User
       class << self
-
         def create(user_data)
           add_to_collection!(
             user_from(user_data)
@@ -13,6 +10,10 @@ module Models
 
         def delete!(user_id)
           collection[user_id] = nil
+        end
+
+        def delete_all!
+          all.each { |user| delete!(user[:id]) }
         end
 
         def find_by_id(user_id)
@@ -29,6 +30,34 @@ module Models
 
         def all
           collection.compact
+        end
+
+        def add_organization(user_id, org_id)
+          find_by_id(user_id).tap do |user|
+            user[:organizations] << org_id
+            user
+          end
+        end
+
+        def remove_organization(user_id, remove_org_id)
+          find_by_id(user_id).tap do |user|
+            user[:organizations] = user[:organizations].reject { |org_id| org_id == remove_org_id }
+            user
+          end
+        end
+
+        def add_role(user_id, role_id)
+          find_by_id(user_id).tap do |user|
+            user[:roles] << role_id
+            user
+          end
+        end
+
+        def remove_role(user_id, remove_role_id)
+          find_by_id(user_id).tap do |user|
+            user[:roles] = user[:roles].reject{ |role_id| role_id == remove_role_id }
+            user
+          end
         end
 
         private
@@ -51,7 +80,8 @@ module Models
         end
 
         def next_id
-          collection.size + 1
+          return collection.size + 1 if collection.size == 0
+          collection.size
         end
       end
     end
---
+++ spec/part2/question2/models/memory/user_spec.rb
@@ -0,0 +1,137 @@
+require 'spec_helper'
+require 'part2/question2/models/memory/user'
+
+describe Models::Memory::User do
+  subject { described_class }
+
+  let(:user) do
+    {
+      name: "Test User",
+      organizations: [],
+      roles: []
+    }
+  end
+
+  before(:each) do
+    subject.all.each { |record| subject.delete!(record[:id]) }
+  end
+
+  context 'create' do
+    it 'creates a new user record' do
+      actual_user   = subject.create(user)
+      expected_user = subject.all.last
+
+      actual_user.should == expected_user
+    end
+
+    it 'creates a new user with a role' do
+      role_ids = [1]
+      test_user = subject.create(user.merge(roles: role_ids))
+      test_user[:roles].should == role_ids
+    end
+
+    it 'creates a new user with an organization' do
+      org_ids = [1]
+      test_user = subject.create(user.merge(organizations: org_ids))
+      test_user[:organizations].should == org_ids
+    end
+  end
+
+  context 'delete' do
+    it 'deletes an existing record' do
+      test_user = subject.create(user)
+      subject.delete!(test_user[:id])
+
+      subject.all.should be_empty
+    end
+
+    it 'deletes all existing records' do
+      subject.create(user)
+      subject.create(user)
+
+      subject.delete_all!
+      subject.all.size.should == 0
+    end
+  end
+
+  context 'all' do
+    it 'returns an empty collection when no records exist' do
+      subject.all.should == []
+    end
+
+    it 'returns one user when one user was created' do
+      test_user = subject.create(user)
+
+      subject.all.should == [test_user]
+    end
+
+    it 'returns users in the order they were added to the collection' do
+      test_user1 = subject.create(user)
+      test_user2 = subject.create(user)
+      test_user3 = subject.create(user)
+
+      subject.all.should == [test_user1, test_user2, test_user3]
+    end
+  end
+
+  context 'find' do
+    it 'finds a user by id' do
+      test_user = subject.create(user)
+
+      subject.find_by_id(test_user[:id]).should == test_user
+    end
+
+    it 'finds organizations by user id' do
+      org_ids = [1]
+      test_user = subject.create(user.merge(organizations: org_ids))
+
+      subject.organizations_for(test_user[:id]).should == org_ids
+    end
+
+    it 'finds roles by user id' do
+      role_ids = [1]
+      test_user = subject.create(user.merge(roles: role_ids))
+
+      subject.roles_for(test_user[:id]).should == role_ids
+    end
+  end
+
+  context 'organizations' do
+
+    it 'adds an organization to a user' do
+      test_user = subject.create(user)
+      new_org_id = 1
+
+      subject.add_organization(test_user[:id], new_org_id)
+      test_user[:organizations].should == [new_org_id]
+    end
+
+    it 'removes an organization from a user' do
+      org_ids = [1]
+      test_user = subject.create(user.merge(organizations: org_ids))
+
+      subject.remove_organization(test_user[:id], org_ids.first)
+
+      test_user[:organizations].size.should == 0
+    end
+  end
+
+  context 'roles' do
+    it 'adds a role to a user' do
+      test_user = subject.create(user)
+      new_role_id = 1
+
+      subject.add_role(test_user[:id], new_role_id)
+      test_user[:roles].should == [new_role_id]
+    end
+
+    it 'removes a role from a user' do
+      role_ids = [1]
+      test_user = subject.create(user.merge(roles: role_ids))
+
+      subject.remove_role(test_user[:id], role_ids.first)
+
+      test_user[:roles].size.should == 0
+    end
+  end
+end
