5cd3ea0f4f0befe8459ebdc8260eddd6f93e01c3
Rick Winfrey <rick.winfrey@gmail.com>
2014-04-15 19:52:46 -0500

    Adds Entities::Organization

---
+++ part2/question2/entities/organization.rb
@@ -0,0 +1,13 @@
+module Entities
+  class Organization < Struct.new(
+                                   :id,
+                                   :name,
+                                   :parent_id,
+                                   :children
+                                 )
+
+    def initialize(from_hash)
+      super(*from_hash.values_at(*members))
+    end
+  end
+end
---
+++ spec/part2/question2/entities/organization_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+require 'part2/question2/entities/organization'
+
+describe Entities::Organization do
+  let(:organization_entity_options) do
+    {
+      id: 1,
+      name: "Test Organization",
+      parent_id: 0,
+      children: []
+    }
+  end
+
+  subject { described_class.new(organization_entity_options) }
+
+  it 'has an id' do
+    subject.id.should == 1
+  end
+
+  it 'has a name' do
+    subject.name.should == "Test Organization"
+  end
+
+  it 'has a parent_id' do
+    subject.parent_id.should == 0
+  end
+
+  it 'has children' do
+    subject.children.should == []
+  end
+end
